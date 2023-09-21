3126dbdc77500d043baf22e85d20d15dbe7ace17
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-21 12:38:04 +0000

    input/output

---
+++ Getting started c++/first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x,b;
+   //cin>>x>>b;
+   cout<<"enter your first no :";
+   
+   cin>>x;
+   cout<<"enter your second no :";
+   cin>>b;
+   cout<<"final result is: "<<x+b<<endl;
+
+    
+}
