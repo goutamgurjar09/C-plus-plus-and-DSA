87b34e623ae9bfae2cd6b97ecaabda95ac7c4c2a
goutam gurjar <goutamgurjar09@gmail.com>
2023-09-21 20:22:50 +0530

    input

---
+++ third.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // std::cout<<"coder army";
+    //cout<<"goutam gurjar"<<endl;
+   // cout<<"goutam gurjar\n"<<"goutam"<<endl;
+    //cout<<"goutam";
+    // cout<<3 + 2;
+    // cout<<3;
+     cout<<"3 * 2";
+
+}
