733a2bad5229b08cb3b59b2a404003e098dd9caa
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-23 12:00:02 +0000

    c++

--- Getting started c++/variable & datatypes/2.cpp
+++ Getting started c++/variable & datatypes/2.cpp
@@ -8,6 +8,27 @@ int main(){
 // cout<<x<<endl;
 // cout<<"x"<<endl;
 
-char c = 'a';
-cout<<c<<endl;
+// char c = 'a';
+// cout<<c<<endl;
+
+// float f = 1.23;
+// cout<<f<<endl;
+
+
+// float e = 1.234567952;
+// cout<<e<<endl;
+// double d = 1.23456789;
+// cout<<d<<endl;
+
+ //error: redeclaration of ‘bool b
+// bool b = 1;
+// bool b = true;
+// bool b = 0;
+// bool b = false;
+// cout <<b;
+//
+
+// bool b = 1;
+// cout<<b<<endl;
+
 }
