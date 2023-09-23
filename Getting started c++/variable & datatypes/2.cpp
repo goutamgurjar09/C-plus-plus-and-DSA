a63adaa6161d30c727b1630d7ff53b293ba655e7
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-23 11:03:30 +0000

    variable & datatype

---
+++ Getting started c++/variable & datatypes/2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+//    int x;//variable declare 
+//    x = 10;//variable define 
+
+// int x = 10;//both work one line
+// cout<<x<<endl;
+// cout<<"x"<<endl;
+
+char c = 'a';
+cout<<c<<endl;
+}
