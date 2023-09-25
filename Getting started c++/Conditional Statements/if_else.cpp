30ed9036380811b0b684df8a8b34480ec9b94097
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-25 10:30:33 +0000

    if_else

---
+++ Getting started c++/Conditional Statements/if_else.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // int package;
+   //  cout<<"enter your package :";
+   //  cin>>package;
+
+   // if(package > 15){
+   //  cout<< "Acceptence";
+   // }
+   // else{
+   //  cout<<"Rejected";
+   // }
+
+   // int marks;
+   //   cout<<"enter your marks :";
+   //  cin>>marks;
+
+   // if(marks > 33){
+   //  cout<< "Pass";
+   // }
+   // else{
+   //  cout<<"Fail";
+   // }
+
+   // int a,b;
+   // cout<<"enter a :";
+   // cin>>a;
+   // cout<<"enter b :";
+   //  cin>>b;
+   // if(a > b){
+   //  cout<< "Yes";
+   // }
+   // else{
+   //  cout<<"No";
+   // }
+
+   // int num;
+   // cout<<"enter your number :";
+   //  cin>>num;
+
+   // if(num % 2 == 0){
+   //  cout<< "Num is Even";
+   // }
+   // else{//num % 2 == 13
+   //   cout<<"Num is odd";
+   // }
+
+   // int age;
+   // cout<<"enter your age :";
+   // cin>>age;
+   
+   // if(age >= 18){
+   //  cout<< "Adult";
+   // }
+   // else{
+   //  cout<<"Younger";
+   // }
+
+   
+   // int num;
+   // cout<<"enter your num :";
+   // cin>>num;
+   
+   // if(num > 0)
+   //  cout<< "Positive";
+   
+   // else if(num < 0)
+   //  cout<<"negative";
+   
+   // else
+   //  cout<<"Zero";
+   
+   
+   // char c;
+   // cout<<"enter your character :";
+   // cin>>c;
+   
+   // if(c == 'a' || c == 'e' || c == 'i' ||  c == 'o' || c == 'u')
+   //  cout<< "Vowel";
+   
+   // else
+   //  cout<<"Consonent";
+
+
+   // int num;
+   // cout<<"enter your num :";
+   // cin>>num;
+   
+   // if(num == 1)
+   //  cout<< "Mon";
+   // else if(num == 2)
+   //  cout<<"Tue";
+   // else if(num == 3)
+   // cout<<"Wed";
+   // else if(num == 4)
+   // cout<<"Thu";
+   // else if(num == 5)
+   // cout<<"Fri";
+   // else if(num == 6)
+   // cout<<"Sat";
+   // else if(num == 7)
+   //  cout<<"Sun";
+   // else
+   //  cout<<"pls enter valid day";
+
+}
+ 
