37c3aa066519ee594e0572c08eedb957b77903fc
Goutam gurjar <goutamgurjar09@gmail.com>
2023-10-16 17:29:59 +0530

    c++

--- Getting started c++/typecasting/1.cpp
+++ Getting started c++/typecasting/1.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(){
-   //int a = 32;
-   //int a = 65; //A
-   int a = 6554;
-    //random value kuch bhi dega bcz hm ise char me change kr rhe or char ka size 8bit hota hai to bo maximum 255 char/value store kr skta hai isse jyada krege to kuch bi random value dega
-    //or hm int-32bit ko char me dal rhe to data to loss  hoga bcz char only 8bit size leta hai
-   char ch = 'b';
-   //a = ch; //b->98 ascii val
+   //char a = 55;  ->7
    //cout<<a<<endl;
+
+//--------------------------
+   
+   //int a = 65; //A
+   // char ch = 'b';
+   // a = ch; //b->98 ascii val
+   // cout<<a<<endl;
+
+   
+ //----------------------  
+   int a = 500; //passing to large value
+   char ch = 'b';
    ch = a; 
    cout<<ch<<endl;
+    //Note:-//ê  means random value kuch bhi dega bcz hm ise char me change kr rhe or char ka size 8bit hota hai to bo maximum 255 char/value store kr skta hai isse jyada krege to kuch bi random value dega
+    //or hm int-32bit ko char me dal rhe to data to loss  hoga bcz char only 8bit size leta hai
+   
+
+
    //a=10 print space bcz main ascii val start with 33 
-   //or ydi hm a ki value 1-32 tk rkhege to ye kuch nhi(space) print krega 
-   //0-255 tk hoti hai ascii val
+   //or ydi hm a ki value 1-32 tk rkhege to ye kuch nhi(space)/kuch bhi print krega 
+   // 0-255 tk hoti hai ascii val
 
 
     
