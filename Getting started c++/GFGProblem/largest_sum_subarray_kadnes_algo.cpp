51871f30d8032b4405db806ee58d7c9f417c3977
Goutam gurjar <goutamgurjar09@gmail.com>
2023-11-08 01:00:49 +0530

    c++

---
+++ Getting started c++/GFGProblem/largest_sum_subarray_kadnes_algo.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long maxSubarrSum(int arr[],int n){
+      int maxi = INT_MIN;
+    
+    //bruite force O(n^2)
+    //   for(int i=0; i<n; i++){
+    //     int prefix = 0;
+    //     for(int j=i; j<n; j++){
+    //         prefix += arr[j];
+    //         maxi = max(prefix,maxi);
+    //     }
+    //   }
+
+    int prefixSum = 0;
+    for(int i=0; i<n; i++){
+      prefixSum += arr[i];
+      maxi = max(prefixSum,maxi);
+      if(prefixSum < 0){//negative
+        prefixSum = 0;
+      }
+    }
+    return maxi;
+}
+int main(){
+  int n;
+  cin>>n;
+  
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+
+   int res = maxSubarrSum(arr,n);
+   cout<<res;
+}
+// 4
+// 4 -6 2 8
+// 10
